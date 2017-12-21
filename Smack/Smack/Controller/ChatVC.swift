eeb0cf139c63fb0bfa9927054938443e145ebb09
Nuno Nunes <nunonunes@Nunos-iMac.local>
2017-12-21 00:42:40 +0000

    end of lesson 1

---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Nuno Nunes on 19/12/2017.
+//  Copyright © 2017 Nuno Nunes. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
