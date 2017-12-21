5da0df8fcb3e167f666cb43ac65894346079c6b9
Nuno Nunes <nunonunes@Nunos-iMac.lan>
2017-12-21 23:44:52 +0000

    end of lesson 4

---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Nuno Nunes on 21/12/2017.
+//  Copyright © 2017 Nuno Nunes. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
